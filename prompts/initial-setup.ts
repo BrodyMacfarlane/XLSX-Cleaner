b08c4a479b2a2c1b3c1ac86e44172fe1961efe10
Brody Macfarlane <macfarlanebrody@gmail.com>
2023-03-06 12:29:43 -0700

    more pretty

--- prompts/initial-setup.ts
+++ prompts/initial-setup.ts
@@ -74,7 +74,7 @@ const createFiles = async () => {
   for (const file of files) {
     const exists = await validateFileOrDirExists(file.file)
     if (!exists) await createFile(file)
-    if (file.file === 'config.json') Progress.note(`Make sure to setup your column compilations inside of ${colors.bold('config.json')}`)
+    if (file.file === 'config.json') Progress.note(`Make sure to setup your column compilations inside of ${colors.cyan('config.json')}`)
   }
 }
 
